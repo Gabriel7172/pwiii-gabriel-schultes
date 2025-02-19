a77e813b572cce54dfaf8e0eeadd57ac726a8365
Gabriel7172 <schultesgabrieltony@gmail.com>
2025-02-19 17:11:18 -0300

    exemplosPHP

---
+++ exemplos/programa4.php
@@ -0,0 +1,24 @@
+<html>
+<head>
+ <title>Engenharia aeroespacial</title>
+</head>
+<body bgcolor="dark blue">
+<H1> Atualmente o ITA completou com sucesso um projeto de lançamento de um microsatelite</H1>
+<BR><BR>
+<?php
+//Linha de comentario
+//Linha ignorada pelo servidor php
+/* testando mais de 
+uma linha de
+comentario
+*/
+
+echo "Engenharia aeroespacial";
+echo "<h1> Atualmente o ITA completou com sucesso um projeto de lançamento de um microsatelite</h1>";
+echo "<BR><BR><center>pocas ideias</center>";
+?>
+
+
+
+</body>
+</html>
